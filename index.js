acb9703645767d698a1398eaa4738aea633f6ea9
Pit Yersh <pitnkz1@gmail.com>
2025-01-06 21:13:49 +0700

    lastlast

--- index.js
+++ index.js
@@ -58,16 +58,20 @@ const start = async () => {
     bot.on("callback_query", async msg => {
         const data = msg.data;
         const chatId = msg.message.chat.id;
+        const user = await UserModels.findOne({chatId})
         if (data !== "/again") {
             await bot.sendMessage(chatId, `Ты нажал кнопку под цыфрой ${data}`);
+            user.wrong += 1;
         }
         if (chats[chatId].toString() === msg.data) {
-            return bot.sendMessage(chatId, "Бля, угадал!", againOptions)
+            user.right += 1;
+            await bot.sendMessage(chatId, "Бля, угадал!", againOptions)
         }
         if (data === "/again") {
-            return startGame(chatId);
+            await startGame(chatId);
         }
-        return bot.sendMessage(chatId, "Пробуй еще", againOptions)
+        await user.save();
+        await bot.sendMessage(chatId, "Пробуй еще", againOptions)
     })
 
 }
